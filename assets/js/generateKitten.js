da502c7274f6cbc40e56055855c586d42eddeea9
MadsenAMS <andre-madsen@hotmail.com>
2021-12-18 11:40:13 -0300

    First Commit

---
+++ assets/js/generateKitten.js
@@ -0,0 +1,15 @@
+const IMAGE = document.getElementById("OverwriteMe");
+const BASE_URL = "https://thatcopy.pw/catapi/rest/";
+
+async function ChangeKitten() {
+  try {
+    const API_RETURN = await fetch(BASE_URL);
+    console.log(API_RETURN);
+    const JSON = await API_RETURN.json();
+    IMAGE.src = JSON.webpurl;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+ChangeKitten();
